037d56721a33cd7c00f8fbf69c18ac7ea7d1402d
julia sheremet <juliajessicasher@gmail.com>
2018-06-13 19:58:51 -0700

    create a method to take an input event in the search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <p>Search: <input /></p>;
-};
+// const SearchBar = () => {
+//   return <p><input placeholder="search"/></p>;
+// };
+
+//onChange is a react property
+
+class SearchBar extends Component { //creating an object
+  render() { //method
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event){
+    // console.log(event.target.value);
+  }
+}
+
 
 export default SearchBar;
 // ReactDOM.render();
